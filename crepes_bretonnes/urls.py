dd886699638a3bbe464c0e8ef46c752c193958e2
RCosson <romain.cosson11@live.fr>
2017-06-22 14:47:54 +0200

    include

--- crepes_bretonnes/urls.py
+++ crepes_bretonnes/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from blog import views
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accueil$', views.home),
+    url(r'^blog/', include('blog.urls')),
 ]
