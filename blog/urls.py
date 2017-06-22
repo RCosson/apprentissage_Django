d91f29f1029804dbf311023a9de4e31b9d3c42c9
RCosson <romain.cosson11@live.fr>
2017-06-22 16:42:13 +0200

    vues

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^accueil$', views.home),
+    url(r'^article/(\d+)$', views.view_article),
+    url(r'^articles/(\d{4})/(\d{2})$', views.list_articles),
+    url(r'^redirection$', views.view_redirection),
+    url(r'^article/(?P<id_article>\d+)$', views.view_article, name="afficher_article"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,30 @@ def home(request):
     text = """<h1>Bienvenue sur mon blog !</h1>
               <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
     return HttpResponse(text)
+
+def list_articles(request, month, year):
+    """ Liste des articles d'un mois précis. """
+    return HttpResponse(
+        "Vous avez demandé les articles de {0} {1}.".format(month, year)
+    )
+
+from django.http import HttpResponse, Http404
+
+def view_article(request, id_article):
+    # Si l'ID est supérieur à 100, nous considérons que l'article n'existe pas
+    if int(id_article) > 100:
+        raise Http404
+
+    return HttpResponse('<h1>Mon article ici</h1>')
+
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect
+
+def view_article(request, id_article):
+    if int(id_article) > 100:
+        raise Http404
+
+    return redirect(view_article, id_article=42)
+
+def view_redirection(request):
+    return HttpResponse("Vous avez été redirigé.")
