eec34af664516cedf9e8dc60f18b99517c53c2fb
RCosson <romain.cosson11@live.fr>
2017-06-23 16:30:28 +0200

    test template

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^accueil$', views.home),
     url(r'^article/(\d+)$', views.view_article),
     url(r'^articles/(\d{4})/(\d{2})$', views.list_articles),
-    url(r'^redirection$', views.view_redirection),
+    #url(r'^redirection$', views.view_redirection),
     url(r'^article/(?P<id_article>\d+)$', views.view_article, name="afficher_article"),
+    url(r'^date$', views.date_actuelle),
+    url(r'^addition/(?P<nombre1>\d+)/(?P<nombre2>\d+)/$', views.addition)
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from datetime import datetime
 
 def home(request):
     """ Exemple de page HTML, non valide pour que l'exemple soit concis """
@@ -20,17 +22,12 @@ def list_articles(request, month, year):
         "Vous avez demandé les articles de {0} {1}.".format(month, year)
     )
 
-from django.http import HttpResponse, Http404
-
 def view_article(request, id_article):
     # Si l'ID est supérieur à 100, nous considérons que l'article n'existe pas
     if int(id_article) > 100:
         raise Http404
 
     return HttpResponse('<h1>Mon article ici</h1>')
-
-from django.http import HttpResponse, Http404
-from django.shortcuts import redirect
 
 def view_article(request, id_article):
     if int(id_article) > 100:
@@ -40,3 +37,14 @@ def view_article(request, id_article):
 
 def view_redirection(request):
     return HttpResponse("Vous avez été redirigé.")
+
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+
+def addition(request, nombre1, nombre2):
+    total = int(nombre1) + int(nombre2)
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
